3a09f78a4983398dde6489a071d655ec770d7ab0
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2026-07-23 20:30:31 -0300

    feat(shell): switch to AppWebShellNarrow below 1024px on web

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,21 +1,25 @@
 import { Slot, usePathname } from 'expo-router';
 import { AppWebShell } from '../../components/shell/app-web-shell.jsx';
+import { AppWebShellNarrow } from '../../components/shell/app-web-shell-narrow.jsx';
 import { AppMobileShell } from '../../components/shell/app-mobile-shell.jsx';
 import { AppLoadingScreen } from '../../components/shell/app-loading-screen.jsx';
 import { isWeb } from '../../utils/platform.js';
 import { useAuthStore } from '../../store/auth-store.js';
+import { useIsNarrowWeb } from '../../hooks/use-is-narrow-web.js';
 
 export default function TabsLayout() {
   const pathname = usePathname();
   const hydrated = useAuthStore((s) => s.hydrated);
+  const isNarrowWeb = useIsNarrowWeb();
 
   if (!hydrated) return <AppLoadingScreen />;
 
   if (isWeb) {
+    const WebShell = isNarrowWeb ? AppWebShellNarrow : AppWebShell;
     return (
-      <AppWebShell pathname={pathname}>
+      <WebShell pathname={pathname}>
         <Slot />
-      </AppWebShell>
+      </WebShell>
     );
   }
 
