a7a7b2fb9f2750af861a5caaa1a2f3bafd2ba293
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2026-09-02 21:58:21 -0300

    feat(payments): add unlisted payments testbed screen

---
+++ app/(tabs)/profile/payments-testbed.jsx
@@ -0,0 +1,5 @@
+import { PaymentsTestbedScreen } from '../../../components/payments/payments-testbed-screen.jsx';
+
+export default function ProfilePaymentsTestbed() {
+  return <PaymentsTestbedScreen />;
+}
